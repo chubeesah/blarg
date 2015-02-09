f92e24b95ffd75c058ce2b1cc8f0421395cd7837
Chubes <Chubes@andrew-mercers-macbook.local>
2015-02-09 13:31:16 -0500

    thought I pushed this

--- lib/blarg.rb
+++ lib/blarg.rb
@@ -2,7 +2,7 @@ require "blarg/version"
 require 'pry'
 require 'camping'
 
-BLOG_REPO = '/Users/brit/projects/improvedmeans/'
+BLOG_REPO = '/Users/Chubes/labs/guessr/lib/blarg/improvedmeans/'
 
 Camping.goes :Blarg
 
@@ -36,6 +36,51 @@ module Blarg
       def self.down
         rename_column Post.table_name, :date, :written
       end
+    end
+  end
+end
+
+def find_controller(request)
+  @routing.each do |route, controller|
+    if request.url =~ route
+      @controller = controller.new(request)
+    end
+  end
+end
+
+module Blarg::Controllers
+  
+  class PostId < R '/posts/(\d+)'
+    def get(id)
+      post = Blarg::Models::Post.find(id)
+      post.to_json
+    end
+
+    def put(id)
+
+    end
+
+    def delete(id)
+      post = Blarg::Models::Post.find(id)
+      post.destroy
+      @status = 204
+      ""
+    end
+  end
+
+  class PostController < R '/posts'
+    def get
+
+    end
+
+    def post
+      new_post = Blarg::Models::Post.create(@input)
+      new_post.to_json
+    end
+
+  class Intro < R '/welcome/to/the/([^/]+)'
+    def get
+    "This is the intro controller"
     end
   end
 end
@@ -184,6 +229,22 @@ class BlogApp
       BlogApp.quit_handler
     end
   end
+end
+
+def top_tags
+  result = {}
+  Blarg::Models::Post.find_each do |p|
+    result[p] = 0
+    p.tags.each do |t|
+      result[p] += 1
+    end
+  end
+  top_tags = result.sort_by{ |k, v| -v }.first(10)
+end
+
+def top_months
+  sqlite_month = "strftime (%m, date)"
+  grouped = Blarg::Models::Post.
 end
 
 def Blarg.create
